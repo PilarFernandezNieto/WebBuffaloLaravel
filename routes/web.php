0e9f5e9576193cc2282d16ac8798238af617da87
PilarFernandezNieto <apreilmc@gmail.com>
2024-11-10 12:57:45 +0100

    cambio dashboard-admin

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/admin', function () {
+    return view('admin');
+})->middleware(['auth', 'verified'])->name('admin');
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
