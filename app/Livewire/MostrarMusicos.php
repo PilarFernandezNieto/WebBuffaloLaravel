75c97b397b99cf495bdaa6a11c0d7480315d97d6
PilarFernandezNieto <apreilmc@gmail.com>
2024-11-11 23:01:44 +0100

    listado de musicos

--- app/Livewire/MostrarMusicos.php
+++ app/Livewire/MostrarMusicos.php
@@ -2,12 +2,16 @@
 
 namespace App\Livewire;
 
+use App\Models\Musico;
 use Livewire\Component;
 
 class MostrarMusicos extends Component
 {
     public function render()
     {
-        return view('livewire.mostrar-musicos');
+        $musicos = Musico::all();
+        return view('livewire.mostrar-musicos', [
+            'musicos' => $musicos
+        ]);
     }
 }
--- resources/views/admin/musicos/index.blade.php
+++ resources/views/admin/musicos/index.blade.php
@@ -7,16 +7,15 @@
 
     <div class="md:py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("Musicos") }}
+            @if (session()->has('mensaje'))
+                <div class="uppercase border border-green-600 bg-green-100 text-green-600 font-bold p-2 m-3">
+                    {{ session('mensaje') }}
                 </div>
-                @if (session()->has('mensaje'))
-                    <div class="uppercase border border-green-600 bg-green-100 text-green-600 font-bold p-2 m-3">
-                        {{session('mensaje')}}
-                    </div>
-                @endif
-            </div>
+            @endif
+
+            <livewire:mostrar-musicos />
+
+
             <x-button-link :href="route('admin.musicos.create')" class="mt-5">Nuevo Músico</x-button-link>
 
         </div>
