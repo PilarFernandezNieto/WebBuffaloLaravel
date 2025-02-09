68f42efa02b70310b6a151ecc2e77db430cdc081
PilarFernandezNieto <apreilmc@gmail.com>
2025-02-09 12:17:14 +0100

    historia completa

--- resources/views/paginas/historia.blade.php
+++ resources/views/paginas/historia.blade.php
@@ -7,7 +7,7 @@
                 <span class="flex justify-end text-xs text-slate-600">Foto de Manfred</span>
                 <img src="/img/risas_baja.jpg" alt="Buffalos" class="max-h-[70rem] overflow-hidden w-full">
             </div>
-            <div class="space-y-8 my-8">
+            <div class="space-y-6 my-8">
                 <p class="text-base leading-7"><b>The Electric Buffalo</b> (Oviedo) surge, como idea, en un garito de
                     costumbres
                     rockeras allá por el año
@@ -35,46 +35,93 @@
         </section>
 
         <!-- SEGUNDO BLOQUE -->
-        <section class="grid md:grid-cols-2 gap-8 items-end">
-            <!-- Columna izquierda -->
-            <div class="md:grid md:grid-rows-2 gap-8">
+        <section class="grid grid-cols-1 lg:grid-cols-2 gap-8 items-start my-8">
+
+            <!-- Columna izquierda (Imagen vertical) -->
+            <div>
+                <span class="flex justify-end text-xs text-slate-600">Foto de Manfred</span>
+                <img src="/img/TrioJovenVertical.jpg" alt="Foto vertical"
+                    class="w-full max-h-[40rem] lg:max-h-[70rem] object-cover object-top">
+            </div>
+            <!-- Columna derecha -->
+            <div class="flex flex-col gap-8 h-full justify-between">
                 <!-- Textos -->
-                <div class="space-y-8">
+                <div class="space-y-6">
                     <p class="text-base leading-7">
                         A partir de ahí, fueron largas mañanas de ensayos en Agüera (La Regueras-Asturias) a unos 30
                         minutos en coche de la capital, previo paso por Casa Edelmiro a desayunar religiosamente. Entre
                         2006 y 2009 se fue cociendo el que sería el primer álbum de la banda “Hidin’ from the Butcher”
                         (2009), la primera y única referencia de Ouroboros Records; una autoedición que tuvo a los
-                        mandos
-                        al sumo sacerdote de Eclipse Estudios, el gran Dani “Desmond” Sevillano.
+                        mandos al sumo sacerdote de Eclipse Estudios, el gran Dani “Desmond” Sevillano.
                     </p>
                     <p class="text-base leading-7">
                         Van pasando los años desde la salida del disco y la banda va juntándose en las pocas ocasiones
-                        que
-                        Ilegales y los otros proyectos de Álvaro y Wilón les van dejando. Pero, bueno... esa era la
-                        premisa
-                        básica, ¿no? No rush. Take your time. En 2016 estaba el segundo largo de la banda, “Keepin’ it
-                        warm”
-                        (Boomerang Discos), calentito y a punto de salir del horno, cuando ocurrió lo absolutamente
+                        que Ilegales y los otros proyectos de Álvaro y Wilón les van dejando. Pero, bueno... esa era la
+                        premisa básica, ¿no? No rush. Take your time. En 2016 estaba el segundo largo de la banda,
+                        “Keepin’ it
+                        warm” (Boomerang Discos), calentito y a punto de salir del horno, cuando ocurrió lo
+                        absolutamente
                         inesperado y devastador.
                     </p>
                     <p class="text-base leading-7">La muerte de Alejandro.</p>
                 </div>
-
-                <!-- Imagen horizontal -->
-                <div class="w-full md:flex md:flex-col justify-end">
+                <div>
                     <span class="flex justify-end text-xs text-slate-600">Foto de Manfred</span>
                     <img src="/img/BuffaloBN.jpg" alt="Foto horizontal" class="w-full block">
                 </div>
             </div>
 
-            <!-- Columna derecha -->
-            <div class="w-full">
-                <span class="flex justify-end text-xs text-slate-600">Foto de Manfred</span>
-                <img src="/img/TrioJovenVertical.jpg" alt="Foto vertical" class="w-full max-h-[70rem] object-cover">
+
+        </section>
+
+
+
+        <!-- TERCER BLOQUE -->
+        <section class="grid lg:grid-cols-2 gap-8 items-start">
+
+            <div class="flex flex-col justify-between space-y-6 h-full">
+                <div class="space-y-3 text-base leading-7 text-gray-800">
+                    <p>Jandro “Espina”, un ser de luz y amor, se iba a los 45. Dejaba una mujer y una hija desoladas y
+                        una
+                        innumerable cantidad de amigos con el corazón hecho pedazos.</p>
+                    <p>El disco se presentó en Oviedo con un elenco irrepetible de compañeros, currantes de las cuatro
+                        cuerdas, con predisposición total a echar un cable. Fueron días de muchos ensayos con muchos
+                        amigos, con caras muy largas, pero con muchas ganas de hacer un show irrepetible. Y después de
+                        todo
+                        esto, con tanta emoción desbordada, comenzó un pequeño exilio en el desierto con un futuro algo
+                        incierto.</p>
+                    <p>El exilio espiritual tocó a su fin algunas semanas después, con la decisión de seguir adelante y
+                        con
+                        la llamada a filas de Sergio “Tutu” Rodríguez, bajista de Los Deltonos, fanático de Miles Davis
+                        y
+                        amo
+                        de llaves de “Tutu Estudios” en Corvera, Asturias.</p>
+                    <p>Junto con el fabuloso teclista “Stone” Sam Rodríguez, que se une a la banda unos meses después,
+                        empieza el nuevo periplo del Búfalo Eléctrico que desemboca en la salida de su tercer trabajo
+                        discográfico dedicado, como no podía ser de otra manera, a la memoria de Alejandro. “Patrolman”
+                        (Boomerang Discos, 2023), bajo la producción de Sergio “Tutu”, es la culminación de un camino
+                        andado
+                        sin más pretensión que hacer la música y las canciones que les apetece, sin prisa, exactamente
+                        como
+                        empezó todo allá por aquel año 2006.</p>
+                </div>
+
+                <!-- Imagen con pie de foto -->
+                <div>
+                    <span class="block text-right text-xs text-gray-500">Foto de Manfred</span>
+                    <img src="/img/wilon_joven.jpg" alt="Wilon joven" class="w-full">
+
+                </div>
+            </div>
+            <div>
+                <img src="/img/cartel_vertical.jpg" alt="Cartel" class="w-full">
             </div>
         </section>
-        <!-- TERCER BLOQUE -->
+
+
+
+
+        <!-- CUARTO BLOQUE -->
         <section class="my-20">
 
             <h2 class="text-4xl mb-8 font-medium">La banda ahora</h2>
