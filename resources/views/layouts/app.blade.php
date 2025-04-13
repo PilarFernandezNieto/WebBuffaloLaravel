02c5196b8b9638e751808eb9cc14c7d2e42b8e0d
PilarFernandezNieto <apreilmc@gmail.com>
2025-04-13 10:33:06 +0200

    aria label en hamburguer y h1 en Inicio

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,7 +69,7 @@
             <div class=" imagen-header min-h-56 md:min-h-35 relative">
                 <div class="bg-slate-950 opacity-25 absolute top-0 left-0 h-full w-full"></div>
                 <div class="absolute z-10 p-7 bottom-4 md:left-4 lg:left-40 w-4/5 space-y-8 hidden md:block ">
-                    <h2 class="text-custom-white fuente-bufalo text-4xl tracking-[0.5rem]">The Electric Buffalo</h2>
+                    <h1 class="text-custom-white fuente-bufalo text-4xl tracking-[0.5rem]">The Electric Buffalo</h1>
                     <p class="text-custom-red fuente-bufalo text-6xl tracking-[2.5rem] ">Patrolman</p>
                 </div>
             </div>
