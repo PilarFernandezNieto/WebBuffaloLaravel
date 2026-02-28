4f9ff49019d527ceb0a2067fb2f4a6d8be8bfb47
PilarFernandezNieto <apreilmc@gmail.com>
2026-02-28 13:06:58 +0100

    contenido condicional

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,9 @@
 <x-app-layout pageTitle="Inicio">
-    <x-contenido :contenido='$contenido' />
+    @isset($contenido)
+        <x-contenido :contenido="$contenido" />
+    @endisset
 
-    <section class="flex flex-col items-center gap-8 pb-20">
+    <section class="flex flex-col items-center gap-8 py-10">
         <button
             class="pinchaAqui px-12 py-4 rounded bg-custom-red hover:bg-custom-red-darker text-white text-base transition duration-500 ease-in-out">Escúchalo</button>
         <audio controls="" class="audiostyle hidden" controlslist="nodownload">
@@ -9,20 +11,23 @@
         </audio>
         <div class="grid grid-cols-3 gap-10 items-center md:grid-cols-5">
             <a href="https://www.facebook.com/TheElectricBuffalo?locale=es_ES" target="_blank">
-                <img loading="lazy" src="img/logo_facebbok.png" alt="logo-youtube" class="w-16 hover:scale-[1.1] transition duration-500 ease-in-out">
+                <img loading="lazy" src="img/logo_facebbok.png" alt="logo-youtube"
+                    class="w-16 hover:scale-[1.1] transition duration-500 ease-in-out">
             </a>
-            <a href="https://www.youtube.com/@theelectricbuffalo666" target="_blank" >
-                <img loading="lazy" src="img/youtuberecorte.png" alt="logo-youtube" class="w-16 hover:scale-[1.1] transition duration-500 ease-in-out">
+            <a href="https://www.youtube.com/@theelectricbuffalo666" target="_blank">
+                <img loading="lazy" src="img/youtuberecorte.png" alt="logo-youtube"
+                    class="w-16 hover:scale-[1.1] transition duration-500 ease-in-out">
             </a>
-            <a href="https://www.instagram.com/theelectricbuffalo/" target="_blank" >
+            <a href="https://www.instagram.com/theelectricbuffalo/" target="_blank">
                 <img loading="lazy" src="/img/instagram_logo_2022..png" alt="logo-instagram"
                     class="w-16 hover:scale-[1.1] transition duration-500 ease-in-out">
             </a>
             <a href="https://open.spotify.com/intl-es/artist/4ciUFLaycqUBlM162ifmSH?si=eugYIzlWQnqkxGXJMXzYcw"
                 target="_blank">
-                <img loading="lazy" src="/img/spotifty.png" alt="logo-spotify" class="w-16 rounded-lg hover:scale-[1.1] transition duration-500 ease-in-out">
+                <img loading="lazy" src="/img/spotifty.png" alt="logo-spotify"
+                    class="w-16 rounded-lg hover:scale-[1.1] transition duration-500 ease-in-out">
             </a>
-            <a href="https://theelectricbuffalo.bandcamp.com/" target="_blank" >
+            <a href="https://theelectricbuffalo.bandcamp.com/" target="_blank">
                 <img loading="lazy" src="/img/bandcamp-button-circle-aqua-64.png" alt="logo-bandcamp"
                     class="w-16 hover:scale-[1.1] transition duration-500 ease-in-out">
             </a>
@@ -35,11 +40,12 @@
         <div class="py-4 text-center z-10">
             <h3 class="text-white text-4xl mb-12">Discografía del Búfalo Eléctrico</h3>
             <a href="{{ route('discografia') }}"
-                class="bg-custom-red hover:bg-custom-red-darker rounded-md text-white mt-10 px-12 py-4 transition duration-500 ease-in-out">Pincha aquí</a>
+                class="bg-custom-red hover:bg-custom-red-darker rounded-md text-white mt-10 px-12 py-4 transition duration-500 ease-in-out">Pincha
+                aquí</a>
         </div>
     </section>
 
 
-        <livewire:noticias-portada />
+    <livewire:noticias-portada />
 
 </x-app-layout>
